2d8569498816dda1d5b717fa60f0d4a3334ff822
jle <jle@3635fb85-88e8-0310-a153-e985ce46fe7b>
2007-01-19 03:41:18 +0000

    Check directory.

--- c-pluff/autogen.sh
+++ c-pluff/autogen.sh
@@ -1,9 +1,24 @@
 #! /bin/sh
+#
+# Copyright 2007 Johannes Lehtinen
+# 
+# This autogen.sh script is free software; Johannes Lehtinen gives
+# unlimited permission to copy, distribute and modify it.
 
 set -e
 
+# Check directory
+basedir="`dirname "$0"`"
+if ! test -f "$basedir"/libcpluff/cpluff.h; then
+    echo 'Run autogen.sh in the top level source directory.' 1>&2
+    exit 1
+fi
+
+# Generate files in top level directory
+cd "$basedir"
 test -e po/Makefile.in.in || gettextize
 test -e auxliary/ltmain.sh || libtoolize
 aclocal -I m4
+autoheader
 automake -a
 autoconf
