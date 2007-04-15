c4752a054adf0ce98c95c6fe3f24fed3c1007d33
jle <johannes.lehtinen@iki.fi>
2007-04-15 02:43:42 +0000

    Expose cpi_fatalf to the C++ implementation.

--- libcpluff/shared.h
+++ libcpluff/shared.h
@@ -22,48 +22,37 @@
  *-----------------------------------------------------------------------*/
 
 /** @file
- * Core internal defines. These are used for both C and C++ implementation.
+ * Data structures and declarations shared with the C++ API
  */
 
-#ifndef DEFINES_H_
-#define DEFINES_H_
+#ifndef SHARED_H_
+#define SHARED_H_
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-#ifdef ENABLE_NLS
-#include <libintl.h>
-#endif
+#include "defines.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
 
 
 /* ------------------------------------------------------------------------
- * Defines
+ * Function declarations
  * ----------------------------------------------------------------------*/
 
-// Gettext defines 
-#ifdef ENABLE_NLS
-#define _(String) dgettext(PACKAGE, String)
-#define gettext_noop(String) String
-#define N_(String) gettext_noop(String)
-#else
-#define _(String) (String)
-#define N_(String) String
-#define textdomain(Domain)
-#define bindtextdomain(Package, Directory)
-#endif //HAVE_GETTEXT
+/**
+ * Reports a fatal error. This method does not return.
+ * 
+ * @param msg the formatted error message
+ * @param ... parameters
+ * 
+ * @internal
+ * This method is exported for use by the C++ interface.
+ */
+CP_C_API void cpi_fatalf(const char *msg, ...) CP_GCC_NORETURN CP_GCC_PRINTF(1, 2) CP_GCC_NONNULL(1);
 
 
-// Additional defines for function attributes (under GCC). 
-#if (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 5)) && ! defined(printf)
-#define CP_GCC_PRINTF(format_idx, arg_idx) \
-	__attribute__((format (printf, format_idx, arg_idx)))
-#define CP_GCC_CONST __attribute__((const))
-#define CP_GCC_NORETURN __attribute__((noreturn))
-#else
-#define CP_GCC_PRINTF(format_idx, arg_idx)
-#define CP_GCC_CONST
-#define CP_GCC_NORETURN
-#endif
+#ifdef __cplusplus
+}
+#endif //__cplusplus
 
-
-#endif //DEFINES_H_
+#endif /*SHARED_H_*/
