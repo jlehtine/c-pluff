fa254c67fbac42136d1398b409f057ac568c7739
jle <johannes.lehtinen@iki.fi>
2010-05-22 15:34:45 +0000

    Added test case for unregistering plug-in collection after scanning.

--- test/collections.c
+++ test/collections.c
@@ -97,3 +97,22 @@ void unregcollections(void) {
 	cp_destroy();
 	check(errors == 0);
 }
+
+void scanunregcollection(void) {
+	cp_context_t *ctx;
+	int errors;
+	ctx = init_context(CP_LOG_ERROR, &errors);
+	check(cp_register_pcollection(ctx, pcollectiondir("collection1")) == CP_OK);
+	check(cp_register_pcollection(ctx, pcollectiondir("collection2")) == CP_OK);
+	cp_unregister_pcollection(ctx, pcollectiondir("collection2"));
+	check(cp_scan_plugins(ctx, 0) == CP_OK);
+	check(cp_get_plugin_state(ctx, "plugin1") == CP_PLUGIN_INSTALLED);
+	check(cp_get_plugin_state(ctx, "plugin2a") == CP_PLUGIN_UNINSTALLED);
+	check(cp_get_plugin_state(ctx, "plugin2b") == CP_PLUGIN_UNINSTALLED);
+	cp_unregister_pcollection(ctx, pcollectiondir("collection1"));
+	check(cp_get_plugin_state(ctx, "plugin1") == CP_PLUGIN_INSTALLED);
+	check(cp_scan_plugins(ctx, 0) == CP_OK);
+	check(cp_get_plugin_state(ctx, "plugin1") == CP_PLUGIN_INSTALLED);
+	cp_destroy();
+	check(errors == 0);
+}
