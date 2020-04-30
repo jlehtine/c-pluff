1345e0ebf97e920589ee9c063d7beee78e5ba9db
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2020-05-01 02:01:48 +0300

    Remove link to an obsolete site.

--- doc/listconfirm/subscribed-announcement.en.php
+++ doc/listconfirm/subscribed-announcement.en.php
@@ -28,7 +28,7 @@
     </p>
 
     <p class="footer">
-      Copyright 2007 <a href="http://www.jlehtinen.net/">Johannes Lehtinen</a><br/>
+      Copyright 2007 Johannes Lehtinen<br/>
       Validation:
         <a href="http://validator.w3.org/check?uri=referer">XHTML Basic 1.0</a>,
         <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS 2</a>
